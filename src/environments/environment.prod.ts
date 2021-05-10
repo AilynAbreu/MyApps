a33f0c26874a8ad4fade2db55dc58ade6f60dfdd
AilynAbreu <ailynabreu07@gmail.com>
2021-05-10 20:47:47 +0800

    Commit all files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBGsK9Hc88rYynuL5bdtw86turWBlVyqlw",
+    authDomain: "myapps-50f66.firebaseapp.com",
+    databaseURL: "https://myapps-50f66-default-rtdb.firebaseio.com",
+    projectId: "myapps-50f66",
+    storageBucket: "myapps-50f66.appspot.com",
+    messagingSenderId: "614581193285",
+    appId: "1:614581193285:web:ada7afae8e5f876903b12c",
+    measurementId: "G-18CX14HY3L"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBGsK9Hc88rYynuL5bdtw86turWBlVyqlw",
+    authDomain: "myapps-50f66.firebaseapp.com",
+    databaseURL: "https://myapps-50f66-default-rtdb.firebaseio.com",
+    projectId: "myapps-50f66",
+    storageBucket: "myapps-50f66.appspot.com",
+    messagingSenderId: "614581193285",
+    appId: "1:614581193285:web:ada7afae8e5f876903b12c",
+    measurementId: "G-18CX14HY3L"
+  }
 };
 
 /*
